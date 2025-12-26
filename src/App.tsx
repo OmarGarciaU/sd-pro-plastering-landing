b891e34a9e0e28c979408a025742f1d9ab60571f
Omar Garcia <gomar0428@gmail.com>
2025-12-25 22:23:13 -0800

    changed browser router information

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/sd-pro-plastering-landing">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,12 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/sd-pro-plastering-landing' : '/',
+  server: {
+    host: "::",
+    port: 8080,
+  },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: "/sd-pro-plastering-landing",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
